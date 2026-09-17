2bf8582307197e376056ed848ecffed11e41c062
adihex <adihex@users.noreply.github.com>
2026-09-17 19:44:17 +0530

    Move glyphs to Plane-15 PUA (U+F0000+) to avoid Nerd Font collisions

--- tools/build-font.py
+++ tools/build-font.py
@@ -128,7 +128,7 @@ def main():
     for i, svg in enumerate(svgs):
         g, n = svg_to_glyph(svg)
         name = svg.stem
-        cp = 0xE000 + i
+        cp = 0xF0000 + i  # Plane-15 PUA: no collisions with Nerd Fonts etc.
         names.append(name)
         glyphs.append(g)
         cps[name] = cp
